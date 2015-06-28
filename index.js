768783e63742c1710934c61669dcb538ce5c26a3
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2015-06-29 00:27:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var xtend = require('xtend');
+
+var noop = function() {};
+
+var defaults = {
+  path: '.env',
+  loadTo: process.env,
+  override: true
+};
+
+module.exports.load = load;
+
+function load(opts) {
+  if (typeof opts === 'string') {
+    opts = { path: opts };
+  }
+
+  opts = xtend(defaults, opts);
+
+  var file = fs.readFileSync(opts.path, { encoding: 'utf8' });
+  lines(file).forEach(onLine);
+
+  function onLine(line) {
+    var pattern = /^\s*(?:export)?\s*([^=]*)(?:=(.*))?$/i;
+    var match = line.match(pattern);
+    if (match) {
+      assign(opts.loadTo, opts.override, match[1], match[2]);
+    }
+  }
+
+  function assign(obj, override, name, value) {
+    value = value || '';
+
+    var exists = obj[name];
+    if (exists && !override)
+      return;
+
+    obj[name] = cleanQuotes(value);
+  }
+
+  function cleanQuotes(value) {
+    return value.replace(/(^['"]|['"]$)/g, '').trim();
+  }
+
+  function lines(content) {
+    return content.split('\n');
+  }
+
+}
---
+++ test/index.js
@@ -0,0 +1,72 @@
+var assert = require('chai').assert;
+var path = require('path');
+
+var env = require('../');
+
+function file(name) {
+  return path.resolve(__dirname, name);
+}
+
+describe('.env', function() {
+  var loadTo = {};
+
+  beforeEach(function(){
+    loadTo = { };
+  });
+
+  it('throw when do not find the file', function(){
+    assert.throws(function() {
+      env.load({ path: 'not.exist' });
+    }, /ENOENT/);
+  });
+
+  it('load default .env file', function() {
+    env.load({ loadTo: loadTo, path: file('.env') });
+    assert.equal(loadTo.NODE_TEST, 'test');
+    assert.equal(loadTo.DB_URL, 'mongo://localhost:27017');
+    assert.equal(loadTo.API_SECRET, '123131232');
+  });
+
+  it('loads .custom.env file', function() {
+    env.load({ path: file('.custom.env'), loadTo: loadTo });
+    assert.equal(loadTo.CUSTOM, 'custom value');
+  });
+
+  it('override environment variable [ default behavior ]', function() {
+    loadTo.OVERRIDE = 1;
+    env.load({ loadTo: loadTo, path: file('.override.env') });
+    assert.equal(loadTo.OVERRIDE, '2');
+  });
+
+  it('do not override environment variable', function() {
+    loadTo.OVERRIDE = 1;
+    env.load({ loadTo: loadTo, override: false, path: file('.no.override.env') });
+    assert.equal(loadTo.OVERRIDE, 1);
+  });
+
+  it('default to process.env', function() {
+    env.load({ path: file('.env')});
+    assert.equal(process.env.NODE_TEST, 'test');
+    assert.equal(process.env.DB_URL, 'mongo://localhost:27017');
+    assert.equal(process.env.API_SECRET, '123131232');
+  });
+
+  it('loads without bash export keyword', function() {
+    env.load({ loadTo: loadTo, path: file('.no.export.env')});
+    assert.equal(loadTo.NODE_TEST, 'test');
+    assert.equal(loadTo.DB_URL, 'mongo://localhost:27017');
+    assert.equal(loadTo.API_SECRET, '123131232');
+    assert.equal(loadTo.NAME, '.env');
+  });
+
+  it('as syntatic sugar accept file path', function() {
+    env.load(file('.sugar.env'));
+    assert.equal(process.env.NODE_SUGAR, 'sugar');
+  });
+
+  it('do not ignore flag environment variable', function() {
+    env.load(file('.flag.env'));
+    assert.isDefined(process.env.VERBOSE);
+  });
+
+});
